439c6cb7a466e3df9718f6e683cc7f018d8ce407
Pat Hill <pathill@lawn-128-61-60-62.lawn.gatech.edu>
2017-05-13 11:42:45 -0400

    Sam's Updates

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,34 @@
-create database test;
+CREATE DATABASE bykit;
 
-use database;
+USE bykit;
 
-create table users (
-    id int auto_increment not null,
-    email varchar(255) not null,
-    username varchar(255) not null,
-    country var char(255) not null,
-    primary key (id)
+CREATE TABLE users (
+    id int auto_increment NOT NULL,
+    email varchar(255) NOT NULL,
+    username varchar(255) NOT NULL,
+    address varchar(255) NOT NULL,
+    locality varchar(255) NOT NULL,
+    country varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
 
+CREATE TABLE offers (
+	id int auto_increment NOT NULL,
+	email_seller varchar(255) NOT NULL,
+	title varchar(255) NOT NULL,
+	type integer(10) NOT NULL,
+	description varchar(255) NOT NULL,
+	lower_time integer(10) NOT NULL,
+	upper_time integer(10) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE rent (
+	id int auto_increment NOT NULL,
+	email_buyer varchar(255) NOT NULL,
+	offer_id integer(10) NOT NULL,
+	lower_time integer(10) NOT NULL,
+	upper_time integer(10) NOT NULL,
+	PRIMARY KEY (id)
 )
 
