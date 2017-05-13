805cc7e2f1a17d8f2c228728af440bf7e5815f96
Pat Hill <pathill@lawn-128-61-60-62.lawn.gatech.edu>
2017-05-13 13:24:47 -0400

    google maps

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 }
-
+// google maps API key: AIzaSyAxhkimBc8By2IOKHTmsFA4U4KI9qKI5kU :from Samuel
 else {
     connection = mysql.createConnection({
         host: 'localhost',
