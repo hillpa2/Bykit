d9b9eafdb1dfed6e05b8ca25e7c014f58cd196a2
Pat Hill <pathill@Pats-MacBook-Pro.local>
2017-05-19 12:48:58 -0400

    Added Laurie's stuff

---
+++ public/js/main.js
@@ -0,0 +1,59 @@
+
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$header = $('#header'),
+			$banner = $('#banner');
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Dropdowns.
+			$('#nav > ul').dropotron({
+				alignment: 'right'
+			});
+	});
+			// Navigation Button.
+				$(
+					'<div id="navButton">' +
+						'<a href="#navPanel" class="toggle"></a>' +
+					'</div>'
+				)
+					.appendTo($body);
+
+			// Navigation Panel.
+				$(
+					'<div id="navPanel">' +
+						'<nav>' +
+							$('#nav').navList() +
+						'</nav>' +
+					'</div>'
+				)
+					.appendTo($body)
+					.panel({
+						delay: 500,
+						hideOnClick: true,
+						hideOnSwipe: true,
+						resetScroll: true,
+						resetForms: true,
+						side: 'left',
+						target: $body,
+						visibleClass: 'navPanel-visible'
+					});
+
+
+				$window.on('load', function() {
+
+					$banner.scrollwatch({
+						delay:		0,
+						range:		0.5,
+						anchor:		'top',
+						on:			function() { $header.addClass('alt reveal'); },
+						off:		function() { $header.removeClass('alt'); }
+					});
+
+				});	
+		
+			
