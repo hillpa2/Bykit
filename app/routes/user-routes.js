19e4c05955c1ee79156ac9d4326141e4833c90e2
Pat Hill <pathill@Pats-MacBook-Pro.local>
2017-05-19 14:20:31 -0400

    Here

--- app/routes/user-routes.js
+++ app/routes/user-routes.js
@@ -54,6 +54,32 @@ module.exports = function(app) {
   });
 
   // GET route for getting all of the offers
+  app.get("/api/users", function(req, res) {
+    // Write code here to retrieve all of the todos from the database and res.json them
+    // back to the user
+    db.users.findAll({}).then(function(results){
+      res.json(results);
+    });
+  });
+
+  // POST route for saving a new offers. We can create offers with the data in req.body
+  app.post("/api/users", function(req, res) {
+    // Write code here to create a new offer and save it to the database
+    // and then res.json back the new offer to the user
+    db.offers.create({
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+      address: req.body.address,
+      locality: req.body.locality,
+      country: req.body.country,
+    }).then(function(results) {
+      res.json(results);
+    })
+  });
+
+
+  // GET route for getting all of the offers
   app.get("/api/offers", function(req, res) {
     // Write code here to retrieve all of the todos from the database and res.json them
     // back to the user
--- models/newUser.js
+++ models/newUser.js
@@ -2,6 +2,7 @@ module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     email: DataTypes.STRING,
     username: DataTypes.STRING,
+    password: DataTypes.STRING,
     address: DataTypes.STRING,
     locality: DataTypes.STRING,
     country: DataTypes.STRING,
